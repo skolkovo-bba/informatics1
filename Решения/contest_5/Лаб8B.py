3e715717f6b8d18b3dcc8aa84b88ffd901b7e00a
Arduinoev3 <v2072211@yandex.ru>
2023-01-16 14:12:37 +0300

    exam hart ready

--- Решения/contest_5/Лаб8B.py
+++ Решения/contest_5/Лаб8B.py
@@ -1,12 +1,10 @@
-s=int(input())
-a=[int(i) for i in input().split()]
-F=[None]*len(a)
+s = int(input())
+a = [int(i) for i in input().split()]
+F = [None] * len(a)
 for i in range(len(a)):
-    m=0
+    m = 0
     for j in range(i):
-        if F[j]>m and a[j]<a[i]:
-            m=F[j]
-    F[i]=m+1
+        if F[j] > m and a[j] < a[i]:
+            m = F[j]
+    F[i] = m + 1
 print(max(F))
-
-        
